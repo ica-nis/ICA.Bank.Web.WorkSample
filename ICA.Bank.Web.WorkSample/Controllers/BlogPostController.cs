2745e33f5d0a3071c7e79aab8e8cd39ace561b0a
Niklas Isenius <niklas.isenius@ica.se>
2019-12-05 08:36:42 +0100

    Added basic ASP.NET Core / CRA application

---
+++ ICA.Bank.Web.WorkSample/Controllers/BlogPostController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ICA.Bank.Web.WorkSample.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BlogPostController : ControllerBase
+    {
+        private readonly ILogger<BlogPostController> _logger;
+
+        public BlogPostController(ILogger<BlogPostController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+
+        }
+    }
+}
